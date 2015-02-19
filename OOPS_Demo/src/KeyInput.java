f8ead64057f3f7de4a996586e77fcdab28d1a449
Kevin Stubblefield <kevins12.92@gmail.com>
2015-02-19 14:58:54 -0600

    Add Arrow Key movement

--- OOPS_Demo/src/KeyInput.java
+++ OOPS_Demo/src/KeyInput.java
@@ -18,22 +18,22 @@ public class KeyInput implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e)
     {
-        if(e.getKeyCode() == KeyEvent.VK_D){
+        if(e.getKeyCode() == KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_RIGHT){
             if(gameData.getTextBoxQueue().isEmpty() || !gameData.getTextBoxQueue().peek().isPriority()) {
                 right = true;
             }
         } 
-        if(e.getKeyCode() == KeyEvent.VK_A){
+        if(e.getKeyCode() == KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_LEFT){
             if(gameData.getTextBoxQueue().isEmpty() || !gameData.getTextBoxQueue().peek().isPriority()) {
                 left = true;
             }
         }
-        if(e.getKeyCode() == KeyEvent.VK_W){
+        if(e.getKeyCode() == KeyEvent.VK_W || e.getKeyCode() == KeyEvent.VK_UP){
             if(gameData.getTextBoxQueue().isEmpty() || !gameData.getTextBoxQueue().peek().isPriority()) {
                 up = true;
             }
         }
-        if(e.getKeyCode() == KeyEvent.VK_S){
+        if(e.getKeyCode() == KeyEvent.VK_S || e.getKeyCode() == KeyEvent.VK_DOWN){
             if(gameData.getTextBoxQueue().isEmpty() || !gameData.getTextBoxQueue().peek().isPriority()) {
                 down = true;
             }
@@ -51,18 +51,18 @@ public class KeyInput implements KeyListener {
     @Override
     public void keyReleased(KeyEvent e)
     {
-        if(e.getKeyCode() == KeyEvent.VK_D)
+        if(e.getKeyCode() == KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_RIGHT)
         {
             right = false;
         }
-        if(e.getKeyCode() == KeyEvent.VK_A)
+        if(e.getKeyCode() == KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_LEFT)
         {
             left = false;
         }
-        if(e.getKeyCode() == KeyEvent.VK_W){
+        if(e.getKeyCode() == KeyEvent.VK_W || e.getKeyCode() == KeyEvent.VK_UP){
           up = false;
        }
-       if(e.getKeyCode() == KeyEvent.VK_S){
+       if(e.getKeyCode() == KeyEvent.VK_S || e.getKeyCode() == KeyEvent.VK_DOWN){
           down = false;
 
        }
